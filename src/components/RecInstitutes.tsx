1b3278cdb9d84e1a6a63bd568b0076bdb140f075
ArjunThazhath <146722372+arjunthazath@users.noreply.github.com>
2024-09-14 13:57:43 +0530

    ok

--- src/components/RecInstitutes.tsx
+++ src/components/RecInstitutes.tsx
@@ -2,23 +2,51 @@
 import React from 'react';
 import { HoverBorderGradient } from './ui/hover-border-gradient';
 import { Separator } from "@/components/ui/separator"
+import { InfiniteMovingCards } from './ui/infinite-moving-cards';
+import { Highlight } from './ui/hero-highlight';
 
 export default function RecInstitutes() {
+  const items1 = [
+    { image: "/images/logo1.png" },
+    { image: "/images/logo2.png" },
+    { image: "/images/logo3.png" },
+    { image: "/images/logo4.png" },
+    { image: "/images/logo5.png" },
+    { image: "/images/logo6.png" },
+    { image: "/images/logo7.png" },
+    { image: "/images/logo8.png" },
+    { image: "/images/logo9.png" },
+    { image: "/images/logo10.png" },
+  ];
+  const items2 = [
+    { image: "/images/clogo1.png" },
+    { image: "/images/clogo2.png" },
+    { image: "/images/clogo3.png" },
+    { image: "/images/clogo4.png" },
+    { image: "/images/clogo5.png" },
+    { image: "/images/clogo6.png" },
+    { image: "/images/clogo7.png" },
+    { image: "/images/clogo8.png" },
+    { image: "/images/clogo9.png" },
+    { image: "/images/clogo10.png" },
+  ];
+
   return (
-    <div className='bg-transparent min-w-full min-h-screen flex flex-row items-start justify-evenly py-10 relative'>
-      <div className='flex flex-col items-center text-center'>
-        <HoverBorderGradient>
-          <span>Our Institutions</span>
-        </HoverBorderGradient>
+    <div className='bg-transparent min-w-full min-h-screen flex flex-col items-center justify-evenly pt-0 pb-10 relative space-y-5'>
+      <div className='flex flex-col items-center space-y-2 text-center'>
+        <Highlight>
+          <span className='text-3xl py-4 font-black text-black dark:text-white'>Our Institutions</span>
+          </Highlight>
+        <InfiniteMovingCards items={items1} direction="left" />
       </div>
 
       {/* Adding visible height and a noticeable background color */}
-      <Separator orientation="vertical" className="bg-slate-900 dark:bg-yellow-500 h-[400px] w-[2px]" />
-
-      <div>
-        <HoverBorderGradient>
-          <span>Our Recruiters</span>
-        </HoverBorderGradient>
+      
+      <div className='flex flex-col items-center space-y-2 text-center'>
+        <Highlight>
+          <span className='text-3xl py-4 font-black text-black dark:text-white'>Our Recruiters</span>
+        </Highlight>
+        <InfiniteMovingCards items={items2} direction="right" />
       </div>
     </div>
   );
